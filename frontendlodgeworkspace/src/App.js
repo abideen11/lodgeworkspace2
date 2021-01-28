62276faf9662ea522f5f6bd62ace6220a921117d
Abideen Kareem <abideenkareem17@gmail.com>
2021-01-27 20:47:48 -0600

    polish frontend

--- frontendlodgeworkspace/src/App.js
+++ frontendlodgeworkspace/src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
-import Rentals from './RentalContainer';
 import RentalContainer from './RentalContainer';
 import Error from './Error';
 import LogIn from './LogIn';
